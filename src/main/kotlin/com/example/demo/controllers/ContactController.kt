ebef01d9f1100f772463a9d2715d949d63cd2b62
Ismael Borges <ismaelborgessto@gmail.com>
2021-12-02 22:33:45 -0300

    API RestFull Spring and Kotlin

---
+++ src/main/kotlin/com/example/demo/controllers/ContactController.kt
@@ -0,0 +1,48 @@
+package com.example.demo.controllers
+
+import com.example.demo.entities.Contact
+import com.example.demo.repositories.ContactRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import javax.persistence.EntityNotFoundException
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/contacts")
+class ContactController {
+    @Autowired
+    lateinit var repository: ContactRepository
+
+    @GetMapping
+    fun index(): List<Contact> {
+        return repository.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable("id") id: Long): Contact {
+        return repository.findById(id).orElseThrow { EntityNotFoundException() }
+    }
+
+    @PostMapping
+    fun create(@Valid @RequestBody contact: Contact): Contact {
+        return repository.save(contact)
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable("id") id: Long, @Valid @RequestBody newContact: Contact): Contact {
+        val contact = repository.findById(id).orElseThrow { EntityNotFoundException() }
+
+        contact.apply {
+            this.name = newContact.name
+            this.email = newContact.email
+        }
+
+        return repository.save(contact)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable("id") id: Long) {
+        val contact = repository.findById(id).orElseThrow { EntityNotFoundException() }
+        repository.delete(contact)
+    }
+}
